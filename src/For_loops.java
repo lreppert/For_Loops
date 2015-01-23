9e3639cc249b50f2b39814ee0ed0979b496732e4
lreppert <lreppert@nwacc.edu>
2015-01-22 21:33:48 -0600

    Initial commit

---
+++ src/For_loops.java
@@ -0,0 +1,23 @@
+/**
+ * Created by Larry on 1/16/2015.
+ */
+public class For_loops {
+    public static int[][] numArray;
+
+    public static void main(String[] args) {
+        numArray = new int[4][4];
+        numArray[0][0] = 9;
+        numArray[1][3] = 2;
+        numArray[2][1] = 7;
+        numArray[3][3] = 3;
+        numArray[3][0] = 8;
+        for (int i = 0; i < numArray.length; i++) {
+            System.out.println();
+            for (int j = 0; j < numArray[i].length; j++) {
+                System.out.println(numArray[i][j]);
+            }
+        }
+
+    }
+}
+
